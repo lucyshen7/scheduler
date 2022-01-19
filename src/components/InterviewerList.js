a3b176fb964a15eee1a0676050cc7e117ccf05ca
Lucy Shen <lucyshen77@gmail.com>
2022-01-19 10:34:04 -0800

    Built the Interviewer List

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,18 +1,20 @@
 import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
 
-import "components/InterviewerListItem.scss";
+import "components/InterviewerList.scss";
 
 export default function InterviewerList(props) {
 
-  // const { days, day, setDay } = props; // expect a days array to be passed to <DayList> component as a prop
+  const { interviewers, interviewer, setInterviewer } = props; // expect an interviewers array to be passed to <InterviewerList> component as a prop
   
-  // const parsedDays = days.map(item => <DayListItem key={item.id} name={item.name} spots={item.spots} selected={item.name === day} setDay={setDay} />);
+  const parsedInterviewers = interviewers.map(item => <InterviewerListItem key={item.id} name={item.name} avatar={item.avatar} selected={item.id === interviewer} setInterviewer={setInterviewer} />);
 
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">
+        { parsedInterviewers }
+      </ul>
     </section>
   );
 }
