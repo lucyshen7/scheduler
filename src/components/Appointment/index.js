17d1016759070da6aeada7dc2d8e25d6ec2f5c41
Lucy Shen <lucyshen77@gmail.com>
2022-01-19 21:38:33 -0800

    Updated Appointment component to display empty and booked appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,14 +1,19 @@
 import React from "react";
 import "./styles.scss";
 
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+
 export default function Appointment(props) {
 
-  const { time } = props;
+  const { id, time } = props;
 
   return (
     <article className="appointment">
-      { !time && <span>No Appointments</span>}
-      { time && <span>Appointment at {time}</span>}
+      <Header time={time} />
+      {props.interview && <Show student={props.interview.student} interviewer={props.interview.interviewer.name} />}
+      {!props.interview && <Empty />}
     </article>
   )
 }
