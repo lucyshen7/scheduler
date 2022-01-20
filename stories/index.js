d1c93c18efcf6499f0073327b7120779749c0340
Lucy Shen <lucyshen77@gmail.com>
2022-01-19 17:19:52 -0800

    Built a Header Component, Empty Component and added Storybook stories for the components

--- stories/index.js
+++ stories/index.js
@@ -7,7 +7,8 @@ import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment";
-
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
 
 import "index.scss";
 
@@ -143,4 +144,9 @@ storiesOf("Appointment", module)
   })
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
-
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => (
+    <Empty
+      onAdd={action("onAdd")}
+    />
+  ))
