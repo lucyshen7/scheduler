7ab7e3a73b9c17b7d4f1a00ea89bd691393ef0c7
Lucy Shen <lucyshen77@gmail.com>
2022-01-21 16:51:33 -0800

    Implemented our custom useVisualMode Hook to conditionally render the Appointment components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./styles.scss";
 
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 
+import useVisualMode from "hooks/useVisualMode";
+import Form from "./Form";
+
 export default function Appointment(props) {
+  // when props.interview contains a value, pass useVisualMode the SHOW mode
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
 
   const { id, time, interview } = props;
+
+  const { mode, transition, back } = useVisualMode(
+    interview ? SHOW : EMPTY
+  );
+
+  const CREATE = "CREATE";
 
   return (
     <article className="appointment">
       <Header time={time} />
-      {interview ? <Show student={interview.student} interviewer={interview.interviewer.name} /> : <Empty />}
+      {/* {interview ? <Show student={interview.student} interviewer={interview.interviewer.name} /> : <Empty />} */}
+
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === CREATE && <Form interviewers={[]} onCancel={back}/>}
+      {mode === SHOW && (
+        <Show
+          student={interview.student}
+          interviewer={interview.interviewer.name}
+        />
+      )}
     </article>
   )
 }
