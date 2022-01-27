f9d3307f9c60ea853e7a30c957bbadac4fa95f55
Lucy Shen <lucyshen77@gmail.com>
2022-01-26 22:51:43 -0800

    Updated README file

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # Interview Scheduler
 
+## Welcome to my fifth project, Interview Scheduler! 📚
+
+Interview Scheduler is a single-page application built using React (front-end) and Express (back-end). This application allows a student to book an interview with a mentor between Monday and Friday.
+
+Data is persisted by the API server using a PostgreSQL database.
+
+Jest, Storybook and Cypress tests were used throughout the development of the project.
+
+# Final Product
+
+
+# Getting Started
 ## Setup
 
 Install dependencies with `npm install`.
--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -12,9 +12,9 @@ export default function useApplicationData() {
 
   useEffect(() => {
     Promise.all([
-      axios.get('/api/days'),
-      axios.get('/api/appointments'),
-      axios.get('/api/interviewers')
+      axios.get('http://localhost:8001/api/days'),
+      axios.get('http://localhost:8001/api/appointments'),
+      axios.get('http://localhost:8001/api/interviewers')
     ])
       .then(([days, appointments, interviewers]) => {
         setState(prev => ({ ...prev, days: days.data, appointments: appointments.data, interviewers: interviewers.data }));
@@ -57,7 +57,7 @@ export default function useApplicationData() {
 
     const newDays = updateSpots(state, appointments, id);
 
-    return axios.put(`/api/appointments/${id}`, { interview })
+    return axios.put(`http://localhost:8001/api/appointments/${id}`, { interview })
       .then(() => {
         setState(prev => ({ // call setState with new state object
           ...prev,
@@ -81,7 +81,7 @@ export default function useApplicationData() {
 
     const newDays = updateSpots(state, appointments, id);
 
-    return axios.delete(`/api/appointments/${id}`)
+    return axios.delete(`http://localhost:8001/api/appointments/${id}`)
       .then(() => {
         setState(prev => ({ // call setState with new state object
           ...prev,
